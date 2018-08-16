6d74048e098ecc7a74aff4bf11c6b7900d08e493
Nick Ross <nick.ir.ross@gmail.com>
2018-08-16 08:27:51 +0100

    Moved everything to a class

--- imgdd.py
+++ imgdd.py
@@ -1,11 +1,139 @@
 #!/usr/bin/env python
 import argparse
 import filecmp
+import logging
 import os
-from pprint import pprint
 
 import humanize
 from pathquery import pathquery
+
+
+log = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+handler.setLevel(logging.ERROR)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+log.addHandler(handler)
+log.setLevel(logging.ERROR)
+
+
+class ImgDD(object):
+    files = []
+
+    def __init__(self, directories: list):
+        self.files = self._build_file_list(directories)
+
+    @staticmethod
+    def _build_file_list(directories: list) -> list:
+        files = []
+        for directory in directories:
+            for path in pathquery(directory).is_not_dir():
+                files.append(path)
+        return files
+
+    def _group_files_by_size(self) -> dict:
+        files_by_size = {}
+        for file in self.files:
+            size = os.path.getsize(file)
+            if size not in files_by_size:
+                files_by_size[size] = []
+            files_by_size[size].append(file)
+        return {k: v for k, v in files_by_size.items() if len(v) > 1}
+
+    def _find_duplicates(self) -> list:
+        duplicates = []
+        for files in self._group_files_by_size().values():
+            matches = set()
+            for i in range(len(files)):
+                for j in range(i + 1, len(files)):
+                    if filecmp.cmp(files[i], files[j]):
+                        matches.update([files[i], files[j]])
+            if len(matches) > 0:
+                duplicates.append(list(matches))
+        return duplicates
+
+    def dry_run(self):
+        duplicates = self._find_duplicates()
+
+        if len(duplicates) == 0:
+            print('No duplicate files found')
+            return
+
+        total_duplicates = 0
+        total_savings = 0
+        for dupes in duplicates:
+            total_duplicates += (len(dupes) - 1)
+            size = os.path.getsize(dupes[0])
+            total_savings += size * (len(dupes) - 1)
+
+        print(
+            '\nFound {} duplicate file{}\n'
+            'Deleting these duplicates will free up {} of disk space.\n'.format(
+                total_duplicates,
+                's' if total_duplicates != 1 else '',
+                humanize.naturalsize(total_savings)
+            )
+        )
+
+    @staticmethod
+    def is_valid_input_choice(answer, file_count):
+        if len(answer) == 0:
+            return True
+
+        if False in [x.isnumeric() for x in answer]:
+            return False
+
+        selections = list(map(int, answer))
+        if min(selections) < 0 or max(selections) > file_count:
+            return False
+
+        return True
+
+    def prompt_delete_duplicates(self):
+        total_deleted = 0
+        for count, paths in enumerate(self._find_duplicates()):
+            prompt = 'Match {}\n' \
+                     '{}\n\n' \
+                     'Enter the number(s) of the file(s) to be ' \
+                     'deleted (separated by spaces): '.format(
+                        count + 1,
+                        '\n'.join([
+                            '  {}. {}'.format(
+                                count + 1, path
+                            ) for count, path in enumerate(paths)
+                        ])
+                      )
+
+            answer = [x for x in input(prompt).split(' ') if x != '']
+            while not self.is_valid_input_choice(answer, len(paths)):
+                print('\nPlease enter valid numbers separated by spaces')
+                answer = [x for x in input(prompt).split(' ') if x != '']
+
+            if len(answer) == 0:
+                log.info('Skipping Match %s', count + 1)
+                continue
+
+            to_delete = [paths[x - 1] for x in list(map(int, answer))]
+            for file in to_delete:
+                log.debug('Deleting %s', str(file))
+                os.unlink(file)
+                total_deleted += 1
+
+        print('\nDeleted {} duplicate file{}\n'.format(
+            total_deleted, 's' if total_deleted != 1 else ''
+        ))
+
+    def auto_delete_duplicates(self):
+        total_deleted = 0
+        for count, files in enumerate(self._find_duplicates()):
+            for file in files[1:]:
+                log.info('Deleting duplicate %s', file)
+                os.unlink(file)
+                total_deleted += 1
+        print('Deleted {} duplicate file{}'.format(
+            total_deleted, 's' if total_deleted != 1 else ''
+        ))
 
 
 def main():
@@ -25,7 +153,24 @@ def main():
         default=False,
         dest='dry_run'
     )
+    parser.add_argument(
+        '--auto',
+        '-a',
+        action='store_true',
+        default=False,
+        dest='auto'
+    )
+    parser.add_argument(
+        '--verbose',
+        '-v',
+        action='store_true',
+        default=False,
+        dest='verbose'
+    )
     args = parser.parse_args()
+
+    if args.verbose:
+        log.setLevel(logging.DEBUG)
 
     # Get a list of folders
     folders = []
@@ -37,96 +182,13 @@ def main():
         else:
             raise parser.error('{} is not a valid path'.format(folder))
 
-    # Get a list of files
-    files =[]
-    for folder in folders:
-        for path in pathquery(folder).is_not_dir():
-            files.append(path)
-
-    # Group files by size
-    files_by_size = {}
-    for file in files:
-        size = os.path.getsize(file)
-        if size not in files_by_size:
-            files_by_size[size] = []
-        files_by_size[size].append(file)
-
-    # Exclude single files
-    files_by_size = {k: v for k, v in files_by_size.items() if len(v) > 1}
-
-    duplicates = []
-    for files in files_by_size.values():
-        matches = set()
-        for i in range(len(files)):
-            for j in range(i + 1, len(files)):
-                if filecmp.cmp(files[i], files[j]):
-                    matches.update([files[i], files[j]])
-        if len(matches) > 0:
-            duplicates.append(list(matches))
-
-    total_duplicates = sum([len(v) - 1 for v in files_by_size.values()])
-    total_savings = humanize.naturalsize(
-        sum([k * (len(v) - 1) for k, v in files_by_size.items()])
-    )
-
+    imgdd = ImgDD(folders)
     if args.dry_run:
-        print(
-            '\nFound {} duplicate files\n'
-            'Deleting these files would free up {} of disk space.\n'.format(
-                total_duplicates,
-                total_savings
-            )
-        )
-
+        imgdd.dry_run()
+    elif args.auto:
+        imgdd.auto_delete_duplicates()
     else:
-        total_deleted = 0
-        for count, paths in enumerate(duplicates):
-            # input('This is a test')
-            # print('The following duplicates were found....\n')
-
-            prompt = 'Match {}\n' \
-                     '{}\n\n' \
-                     'Enter the number(s) of the file(s) to be ' \
-                     'deleted (separated by spaces): '.format(
-                        count + 1,
-                        '\n'.join([
-                            '  {}. {}'.format(
-                                count + 1, path
-                            ) for count, path in enumerate(paths)
-                        ])
-                      )
-
-            answer = [x for x in input(prompt).split(' ') if x != '']
-            while not is_valid(answer, len(paths)):
-                print('\nPlease enter valid numbers separated by spaces')
-                answer = [x for x in input(prompt).split(' ') if x != '']
-
-            if len(answer) == 0:
-                print('Skipping Match {}'.format(count + 1))
-                continue
-
-            to_delete = [paths[x - 1] for x in list(map(int, answer))]
-            for file in to_delete:
-                os.unlink(file)
-                total_deleted += 1
-
-        print('\nDeleted {} duplicate file{}\n'.format(
-            total_deleted, 's' if total_deleted != 1 else ''
-        ))
-
-
-def is_valid(answer, file_count):
-    if len(answer) == 0:
-        return True
-
-    if False in [x.isnumeric() for x in answer]:
-        return False
-
-    selections = list(map(int, answer))
-    if min(selections) < 0 or max(selections) > file_count:
-        return False
-
-    return True
+        imgdd.prompt_delete_duplicates()
 
 
 if __name__ == '__main__':
